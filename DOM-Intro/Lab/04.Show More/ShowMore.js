eaacdde97ec48e325125f11efc75744e6c379938
lsvelikov <lubomirvelikov@yahoo.com>
2024-06-08 21:35:48 +0200

    DOM
    More solutions

---
+++ DOM-Intro/Lab/04.Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    const textToShow = document.getElementById('text');
+    textToShow.style.display = 'inline';
+    const textToHide = document.getElementById('more');
+    textToHide.style.display = 'none';
+}
